7a4964134ffa02298744b863e637a582a884455b
eryckalves <eryckalves@hotmail.com>
2020-04-18 11:32:45 -0300

    ver 0.7.0 added data sectionOrders

--- src/app/sections/SectionOrders/SectionOrders.component.ts
+++ src/app/sections/SectionOrders/SectionOrders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Order} from '../../shared/order';
 @Component({
   selector: 'app-SectionOrders',
   templateUrl: './SectionOrders.component.html',
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  //Order vem da pasta shared.orders.ts
+  orders: Order[] = [
+    {id: 1,customer:
+      {id:1, name: 'teste1' , email:'teste@example.com', state:'CO'},
+      total: 230 , placed: new Date(2020,12,1), 
+      fulfilled: new Date(2020,1,1),
+      status:'Completed'},
+  ];
 
   ngOnInit() {
   }
